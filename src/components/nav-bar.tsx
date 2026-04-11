2f265a5787615017f57e8d6d51f9d5bcf4f349fa
Jonas Berdoz <jonas.berdoz@gmail.com>
2026-04-11 20:02:18 +0200

    fix title

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function NavBar() {
   return (
     <header className="border-b">
-      <div className="container mx-auto flex h-14 items-center px-4">
+      <div className="flex h-14 items-center px-4">
         <Link href="/" className="text-lg font-semibold">
           The Stuff
         </Link>
